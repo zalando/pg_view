43e1c21c60948ba516ac29325e6cab58326be568
Oleksii Kliukin <oleksii.kliukin@zalando.de>
2016-04-15 10:45:33 +0200

    Compatibility changes for 9.6

--- pg_view.py
+++ pg_view.py
@@ -1373,7 +1373,7 @@ class PgstatCollector(StatCollector):
                       WHERE procpid != pg_backend_pid()
                       GROUP BY 1,2,3,4,5,6,7,9
                 """)
-        else:
+        elif self.dbver < 9.6:
             cur.execute("""
                     SELECT datname,
                            a.pid as pid,
@@ -1422,6 +1422,55 @@ class PgstatCollector(StatCollector):
                       WHERE a.pid != pg_backend_pid()
                       GROUP BY 1,2,3,4,5,6,7,9
                 """)
+        else:
+            cur.execute("""
+                    SELECT datname,
+                           a.pid as pid,
+                           usename,
+                           client_addr,
+                           client_port,
+                           round(extract(epoch from (now() - xact_start))) as age,
+                           CASE WHEN wait_event IS NULL THEN false ELSE true END as waiting,
+                           string_agg(other.pid::TEXT, ',' ORDER BY other.pid) as locked_by,
+                           CASE
+                              WHEN state = 'idle in transaction' THEN
+                                  CASE WHEN xact_start != state_change THEN
+                                      state||' '||CAST( abs(round(extract(epoch from (now() - state_change)))) AS text )
+                                  ELSE
+                                      state
+                                  END
+                              WHEN state = 'active' THEN query
+                              ELSE state
+                              END AS query
+                      FROM pg_stat_activity a
+                      LEFT JOIN pg_locks  this ON (this.pid = a.pid and this.granted = 'f')
+                      -- acquire the same type of lock that is granted
+                      LEFT JOIN pg_locks other ON ((this.locktype = other.locktype AND other.granted = 't')
+                                               AND ( ( this.locktype IN ('relation', 'extend')
+                                                      AND this.database = other.database
+                                                      AND this.relation = other.relation)
+                                                     OR (this.locktype ='page'
+                                                      AND this.database = other.database
+                                                      AND this.relation = other.relation
+                                                      AND this.page = other.page)
+                                                     OR (this.locktype ='tuple'
+                                                      AND this.database = other.database
+                                                      AND this.relation = other.relation
+                                                      AND this.page = other.page
+                                                      AND this.tuple = other.tuple)
+                                                     OR (this.locktype ='transactionid'
+                                                      AND this.transactionid = other.transactionid)
+                                                     OR (this.locktype = 'virtualxid'
+                                                      AND this.virtualxid = other.virtualxid)
+                                                     OR (this.locktype IN ('object', 'userlock', 'advisory')
+                                                      AND this.database = other.database
+                                                      AND this.classid = other.classid
+                                                      AND this.objid = other.objid
+                                                      AND this.objsubid = other.objsubid))
+                                                   )
+                      WHERE a.pid != pg_backend_pid()
+                      GROUP BY 1,2,3,4,5,6,7,9
+            """)
         results = cur.fetchall()
         # fill in the number of total connections, including ourselves
         self.total_connections = len(results) + 1
